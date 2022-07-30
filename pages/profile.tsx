5da672b4f905525a8c8cbbf3e2dd9a98631d206e
pa2777 <pa277@gmail.com>
2022-07-30 07:01:49 +0200

    feat(): add formatMoney

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -7,6 +7,7 @@ import {
 } from "@auth0/nextjs-auth0";
 import styled from "styled-components";
 import {PaymentIntent} from "@stripe/stripe-js";
+import formatMoney from "../lib/formatMoney";
 
 const stripe = new Stripe(`${process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY}`, {
   apiVersion: "2020-08-27",
@@ -41,7 +42,7 @@ export default function Profile({
           {orders.map((order) => (
             <Order key={order.id}>
               <h1>Order Number: {order.id}</h1>
-              <h2>Amount: {order.amount}</h2>
+              <h2>Amount: {formatMoney(order.amount)}</h2>
               <h2>Receipt email: {user.email}</h2>
             </Order>
           ))}
